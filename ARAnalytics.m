86a58626763baa11468670ea4100c38e68c88147
Orta Therox <orta.therox@gmail.com>
2012-12-18 12:08:38 -0500

    init

---
+++ ARAnalytics.m
@@ -0,0 +1,14 @@
+//
+//  ARAnalytics.m
+//  Art.sy
+//
+//  Created by orta therox on 18/12/2012.
+//  Copyright (c) 2012 Art.sy. All rights reserved.
+//
+
+#import "ARAnalytics.h"
+#import "ARAnalytics+GeneratedHeader.h"
+
+@implementation ARAnalytics
+
+@end
