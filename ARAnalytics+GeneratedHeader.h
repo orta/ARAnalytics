601b82750964fa78b16cc9ca9e83eccafd05e25e
Orta Therox <orta.therox@gmail.com>
2013-01-07 17:15:06 -0500

    removed example defines

--- ARAnalytics+GeneratedHeader.h
+++ ARAnalytics+GeneratedHeader.h
@@ -9,29 +9,3 @@
   #define AR_TESTFLIGHT_EXISTS 1 
   
 **/
-
-#import "TestFlight.h"
-#define AR_TESTFLIGHT_EXISTS 1
-
-#import "Mixpanel.h"
-#define AR_MIXPANEL_EXISTS 1
-
-#import "LocalyticsSession.h"
-#define AR_LOCALYTICS_EXISTS 1
-
-#import "Flurry.h"
-#define AR_FLURRY_EXISTS 1
-
-#import "GAI.h"
-#define AR_GOOGLEANALYTICS_EXISTS 1
-
-#import "KISSMetricsAPI.h"
-#define AR_KISSMETRICS_EXISTS 1
-
-#import "Crittercism.h"
-#define AR_CRITTERCISM_EXISTS 1
-
-#define AR_CRASHLYTICS_EXISTS 1
-
-#import "Countly.h"
-#define AR_COUNTLY_EXISTS 1
