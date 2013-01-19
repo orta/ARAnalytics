50660ddeef92b0f0a78ec5d101b84f38180a8def
Orta Therox <orta.therox@gmail.com>
2013-01-19 18:06:39 -0500

    made the generated header the full header

--- ARAnalytics+GeneratedHeader.h
+++ ARAnalytics+GeneratedHeader.h
@@ -1,11 +1,28 @@
-/**
+#import "TestFlight.h"
+#define AR_TESTFLIGHT_EXISTS 1
 
-  I'm an empty file. I'll be replaced by something like this:
-  
-  #import "Mixpanel.h" 
-  #define AR_MIXPANEL_EXISTS 1 
+#import "Mixpanel.h"
+#define AR_MIXPANEL_EXISTS 1
 
-  #import "TestFlight.h" 
-  #define AR_TESTFLIGHT_EXISTS 1 
-  
-**/
+#import "LocalyticsSession.h"
+#define AR_LOCALYTICS_EXISTS 1
+
+#import "Flurry.h"
+#define AR_FLURRY_EXISTS 1
+
+#import "GAI.h"
+#define AR_GOOGLEANALYTICS_EXISTS 1
+
+#import "KISSMetricsAPI.h"
+#define AR_KISSMETRICS_EXISTS 1
+
+#import "Crittercism.h"
+#define AR_CRITTERCISM_EXISTS 1
+
+#define AR_CRASHLYTICS_EXISTS 1
+
+#import "Bugsnag.h"
+#define AR_BUGSNAG_EXISTS 1
+
+#import "Countly.h"
+#define AR_COUNTLY_EXISTS 1
--- ARAnalytics.h
+++ ARAnalytics.h
@@ -26,35 +26,6 @@
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 //  OTHER DEALINGS IN THE SOFTWARE.
 
-#import "TestFlight.h"
-#define AR_TESTFLIGHT_EXISTS 1
-
-#import "Mixpanel.h"
-#define AR_MIXPANEL_EXISTS 1
-
-#import "LocalyticsSession.h"
-#define AR_LOCALYTICS_EXISTS 1
-
-#import "Flurry.h"
-#define AR_FLURRY_EXISTS 1
-
-#import "GAI.h"
-#define AR_GOOGLEANALYTICS_EXISTS 1
-
-#import "KISSMetricsAPI.h"
-#define AR_KISSMETRICS_EXISTS 1
-
-#import "Crittercism.h"
-#define AR_CRITTERCISM_EXISTS 1
-
-#define AR_CRASHLYTICS_EXISTS 1
-
-#import "Bugsnag.h"
-#define AR_BUGSNAG_EXISTS 1
-
-#import "Countly.h"
-#define AR_COUNTLY_EXISTS 1
-
 #import <Foundation/Foundation.h>
 #import "ARAnalytics+GeneratedHeader.h"
 
