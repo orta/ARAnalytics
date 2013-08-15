ecc944adf8db22dee4e52019bb532d134883cdd8
Daniel Haight <confidence.designed@gmail.com>
2013-08-15 20:36:46 +0100

    implement methods for subclass methods for AmplitudeProvider

--- Providers/AmplitudeProvider.m
+++ Providers/AmplitudeProvider.m
@@ -7,7 +7,33 @@
 //
 
 #import "AmplitudeProvider.h"
+#import "Amplitude.h"
 
 @implementation AmplitudeProvider
+
+-(id)initWithIdentifier:(NSString *)identifier {
+    NSAssert([Amplitude class], @"Amplitude is not included");
+    [Amplitude initializeApiKey:identifier];
+    
+    return [super init];
+}
+
+-(void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email {
+    if (userID) {
+        [Amplitude setUserId:@"userId"];
+    }
+    
+    if (email) {
+        [Amplitude setGlobalUserProperties:@{@"email": email}];
+    }
+}
+
+-(void)event:(NSString *)event withProperties:(NSDictionary *)properties {
+    [Amplitude logEvent:event withCustomProperties:properties];
+}
+
+-(void)setUserProperty:(NSString *)property toValue:(NSString *)value {
+    [Amplitude setGlobalUserProperties:@{property: value}];
+}
 
 @end
