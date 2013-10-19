a73962fa02330e89a732bf6b056f0029c24109aa
Daniel Haight <confidence.designed@gmail.com>
2013-10-19 17:47:42 +0100

    add ParseAnalyticsProvider files

---
+++ Providers/ParseAnalyticsProvider.h
@@ -0,0 +1,13 @@
+//
+//  ParseAnalyticsProvider.h
+//  Pods
+//
+//  Created by Daniel Haight on 19/10/2013.
+//
+//
+
+#import "ARAnalyticalProvider.h"
+
+@interface ParseAnalyticsProvider : ARAnalyticalProvider
+
+@end
---
+++ Providers/ParseAnalyticsProvider.m
@@ -0,0 +1,13 @@
+//
+//  ParseAnalyticsProvider.m
+//  Pods
+//
+//  Created by Daniel Haight on 19/10/2013.
+//
+//
+
+#import "ParseAnalyticsProvider.h"
+
+@implementation ParseAnalyticsProvider
+
+@end
