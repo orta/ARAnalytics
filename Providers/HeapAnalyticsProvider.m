b34d78ffaa087e2b4b0da072bfc24d81dcc6081f
Daniel Haight <confidence.designed@gmail.com>
2013-10-20 16:08:36 +0100

    add HeapAnalyticsProvider.{h,m}

---
+++ Providers/HeapAnalyticsProvider.m
@@ -0,0 +1,13 @@
+//
+//  HeapAnalyticsProvider.m
+//  Pods
+//
+//  Created by Daniel Haight on 20/10/2013.
+//
+//
+
+#import "HeapAnalyticsProvider.h"
+
+@implementation HeapAnalyticsProvider
+
+@end
