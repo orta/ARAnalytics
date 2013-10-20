c130eb5fc35c582674c9f0b0f8882bc27a43a4f0
Daniel Haight <confidence.designed@gmail.com>
2013-10-20 16:35:33 +0100

    add user property and event methods

--- Providers/HeapAnalyticsProvider.m
+++ Providers/HeapAnalyticsProvider.m
@@ -19,7 +19,38 @@
     return [super init];
 }
 
+- (void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email {
+    NSDictionary *userDict=@{
+                             @"email" : email,
+                             @"handle" : userID
+                             };
+    
+    [[Heap sharedInstance] identify:userDict];
+    
+}
 
+- (void)setUserProperty:(NSString *)property toValue:(NSString *)value{
+    NSDictionary *userDict=@{
+                             property : value
+                             };
+    
+    [[Heap sharedInstance] identify:userDict];
+    
+}
+
+-(void)event:(id)event withProperties:(id)properties {
+    NSMutableDictionary *props = [NSMutableDictionary new];
+    if (properties) {
+        //values must be strings
+        //https://heapanalytics.com/docs#track
+        //quick and dirty hack to ensure this
+        [properties enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            props[key]=((NSObject *)obj).description;
+        }];
+    }
+    [[Heap sharedInstance] track:event withProperties:props];
+    
+}
 
 #endif
 @end
