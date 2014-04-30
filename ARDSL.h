dd9aa53d6d401604c3cf1742602f57f24410f5cd
Ash Furrow <ash@ashfurrow.com>
2014-04-30 11:37:54 +0200

    Completed events and restructured DSL.

--- ARDSL.h
+++ ARDSL.h
@@ -8,12 +8,20 @@
 
 #import <ARAnalytics/ARAnalytics.h>
 
+// Top-level keys
 extern NSString * const ARAnalyticsTrackedEvents;
 extern NSString * const ARAnalyticsTrackedScreens;
 
-extern NSString * const ARAnalyticsTrackedClassName;
-extern NSString * const ARAnalyticsTrackedLabel;
-extern NSString * const ARAnalyticsTriggeringSelector; // Note: selector must return void
+extern NSString * const ARAnalyticsClass;
+extern NSString * const ARAnalyticsPageName;
+extern NSString * const ARAnalyticsPageNameKeyPath;
+extern NSString * const ARAnalyticsEventName;
+extern NSString * const ARAnalyticsSelectorName; // Note: selector must return void
+extern NSString * const ARAnalyticsEventProperties;
+extern NSString * const ARAnalyticsShouldFire;
+
+typedef NSDictionary*(^ARAnalyticsEventPropertiesBlock)(id instance);
+typedef BOOL(^ARAnalyticsEventShouldFireBlock)(id instance);
 
 @interface ARAnalytics (DSL)
 
--- ARDSL.m
+++ ARDSL.m
@@ -13,15 +13,31 @@
 NSString * const ARAnalyticsTrackedEvents = @"trackedEvents";
 NSString * const ARAnalyticsTrackedScreens = @"trackedScreens";
 
-NSString * const ARAnalyticsTrackedClassName = @"class";
-NSString * const ARAnalyticsTrackedLabel = @"label";
-NSString * const ARAnalyticsTriggeringSelector = @"selector";
+NSString * const ARAnalyticsClass = @"class";
+NSString * const ARAnalyticsPageName = @"pageName";
+NSString * const ARAnalyticsPageNameKeyPath = @"keypath";
+NSString * const ARAnalyticsEventName = @"event";
+NSString * const ARAnalyticsSelectorName = @"selector";
+NSString * const ARAnalyticsEventProperties = @"properties";
+NSString * const ARAnalyticsShouldFire = @"shouldFire";;
 
 Class extractClassFromDictionary (NSDictionary *dictionary) {
-    NSString *className = dictionary[ARAnalyticsTrackedClassName];
-    Class klass = NSClassFromString(className);
+    Class klass = dictionary[ARAnalyticsClass];
     NSCAssert(klass != Nil, @"Class cannot be nil.");
     return klass;
+}
+
+BOOL shouldFireForInstance (NSDictionary *dictionary, id instance) {
+    ARAnalyticsEventShouldFireBlock shouldFireBlock = dictionary[ARAnalyticsShouldFire];
+    
+    BOOL shouldFire;
+    if (shouldFireBlock) {
+        shouldFire = shouldFireBlock(instance);
+    } else {
+        shouldFire = YES;
+    }
+    
+    return shouldFire;
 }
 
 @implementation ARAnalytics (DSL)
@@ -34,20 +50,46 @@ Class extractClassFromDictionary (NSDictionary *dictionary) {
         Class klass = extractClassFromDictionary(object);
         
         SEL selector;
-        NSString *selectorName = object[ARAnalyticsTriggeringSelector];
+        NSString *selectorName = object[ARAnalyticsSelectorName];
         if (selectorName) {
             selector = NSSelectorFromString(selectorName);
             NSAssert(selector != NULL, @"Custom selector lookup failed. ");
         } else {
+#if TARGET_OS_IPHONE
             selector = @selector(viewDidAppear:);
+            NSAssert([[klass new] isKindOfClass:[UIViewController class]], @"Default selector of viewDidAppear: must only be used on classes extending UIViewController. ");
+#else
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:@"You must specify a selector name for page views on OS X."
+                                         userInfo:nil];
+#endif
         }
         
-        NSString *label = configurationDictionary[ARAnalyticsTrackedLabel];
+        // Try to grab the page name from the dictionary.
+        NSString *dictionaryPageName = configurationDictionary[ARAnalyticsPageName];
+        
+        // If there wasn't one, then try to invoke keypath.
+        NSString *pageNameKeypath = configurationDictionary[ARAnalyticsPageNameKeyPath];
         
         RSSwizzleClassMethod(klass, @selector(alloc), RSSWReturnType(id), void, RSSWReplacement({
             id instance = RSSWCallOriginal();
+            __weak __typeof(instance) weakInstance = instance;
             [[instance rac_signalForSelector:selector] subscribeNext:^(id _) {
-                [ARAnalytics pageView:label];
+                id instance = weakInstance;
+                
+                BOOL shouldFire = shouldFireForInstance(configurationDictionary, instance);
+                
+                if (shouldFire) {
+                    NSString *pageName;
+                    if (dictionaryPageName) {
+                        pageName = dictionaryPageName;
+                    } else {
+                        pageName = [instance valueForKeyPath:pageNameKeypath];
+                        NSAssert(pageName, @"Value for Key on `%@` returned nil.", pageNameKeypath);
+                    }
+                    
+                    [ARAnalytics pageView:pageName];
+                }
             }];
             return instance;
         }));
@@ -57,17 +99,29 @@ Class extractClassFromDictionary (NSDictionary *dictionary) {
     [trackedEvents enumerateObjectsUsingBlock:^(id object, NSUInteger idx, BOOL *stop) {
         Class klass = extractClassFromDictionary(object);
         
-        NSString *selectorName = object[ARAnalyticsTriggeringSelector];
+        NSString *selectorName = object[ARAnalyticsSelectorName];
         SEL selector = NSSelectorFromString(selectorName);
         NSAssert(selector != NULL, @"Event selector lookup failed. ");
         
-        NSString *label = configurationDictionary[ARAnalyticsTrackedLabel];
+        NSString *event = configurationDictionary[ARAnalyticsEventName];
         
         RSSwizzleClassMethod(klass, @selector(alloc), RSSWReturnType(id), void, RSSWReplacement({
             id instance = RSSWCallOriginal();
+            __weak __typeof(instance) weakInstance = instance;
             [[instance rac_signalForSelector:selector] subscribeNext:^(id _) {
-                //TODO: properties
-                [ARAnalytics event:label withProperties:nil];
+                id instance = weakInstance;
+                
+                BOOL shouldFire = shouldFireForInstance(configurationDictionary, instance);
+                
+                if (shouldFire) {
+                    NSDictionary *properties;
+                    ARAnalyticsEventPropertiesBlock propertiesBlock = configurationDictionary[ARAnalyticsEventProperties];
+                    if (propertiesBlock) {
+                        properties = propertiesBlock(instance);
+                    }
+                    
+                    [ARAnalytics event:event withProperties:properties];
+                }
             }];
             return instance;
         }));
