0632eec422ab7a98d74ca9813a32657cd6f18361
Ash Furrow <ash@ashfurrow.com>
2014-05-02 15:37:18 +0200

    Fixes test maybe?

---
+++ Bootstrap/ARAnalyticsiOSTests/ARAnalyticsDSLTests.m
@@ -0,0 +1,14 @@
+//
+//  ARAnalyticsDSLTests.m
+//  ARAnalyticsTests
+//
+//  Created by Ash Furrow on 2014-05-02.
+//  Copyright (c) 2014 Orta Therox. All rights reserved.
+//
+
+#import <ARAnalytics/ARAnalytics.h>
+#import <ARAnalytics/ARDSL.h>
+
+SpecBegin(ARAnalyticsTests)
+
+SpecEnd
