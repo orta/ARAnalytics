6f1a1c314894437e7e5c09572c276e644dbfb64b
Ash Furrow <ash@ashfurrow.com>
2014-06-10 17:59:41 -0400

    Fixed build.

---
+++ Providers/UMengAnalyticsProvider.h
@@ -0,0 +1,12 @@
+//
+//  UMengAnalyticsProvider.h
+//  ARAnalyticsTests
+//
+//  Created by Cai Guo on 08/05/2014.
+// 
+//
+
+#import "ARAnalyticalProvider.h"
+
+@interface UMengAnalyticsProvider : ARAnalyticalProvider
+@end
