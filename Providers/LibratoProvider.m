2f9836da0eca18de2c1094d3bcbf9199cfd883d6
Param Aggarwal <paramaggarwal@gmail.com>
2014-07-24 12:36:48 +0530

    New Librato Provider

---
+++ Providers/LibratoProvider.m
@@ -0,0 +1,38 @@
+//
+//  LibratoProvider.m
+//  Pods
+//
+//  Created by Param Aggarwal on 24/07/2014.
+//
+//
+
+#import "LibratoProvider.h"
+
+@interface LibratoProvider ()
+
+@property Librato *client;
+
+@end
+
+@implementation LibratoProvider
+#ifdef AR_LIBRATO_EXISTS
+
+- (instancetype)initWithEmail:(NSString *)email token:(NSString *)token prefix:(NSString *)prefix {
+    NSAssert([Librato class], @"Librato is not included");
+    
+    self.client = [[Librato alloc] initWithEmail:email token:token prefix:prefix];
+    
+    return [super init];
+}
+
+- (void)logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval properties:(NSDictionary *)properties {
+    
+    // convert metric to milliseconds
+    NSNumber *metricValue = [NSNumber numberWithInteger:roundf(interval.floatValue*1000)];
+    
+    LibratoMetric *metric = [LibratoMetric metricNamed:event valued:metricValue];
+    [self.client add:metric];
+}
+
+#endif
+@end
