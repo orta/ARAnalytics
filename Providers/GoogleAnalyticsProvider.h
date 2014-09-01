01785c1977ed8320bbbfcbbe034ec19b4865123d
Brio Taliaferro <briomusic@gmail.com>
2014-09-01 20:13:10 +0100

    fix header docs

--- Providers/GoogleAnalyticsProvider.h
+++ Providers/GoogleAnalyticsProvider.h
@@ -11,10 +11,15 @@
 @interface GoogleAnalyticsProvider : ARAnalyticalProvider
 
 /***
- * These dictionaries map custom names to GAI customDimensions and customMetrics
+ * maps custom names to GAI customDimensions
  * i.e. @{@"FooDimension":[GAIFields customDimensionForIndex:1], @"BarDimension":[GAIFields customDimensionForIndex:2]}
  */
 @property (nonatomic, strong) NSDictionary *customDimensionMappings;
+
+/***
+ * maps custom names to GAI customMetrics
+ * i.e. @{@"FooMetric":[GAIFields customMetricForIndex:1], @"BarMetric":[GAIFields customMetricForIndex:2]}
+ */
 @property (nonatomic, strong) NSDictionary *customMetricMappings;
 
 
