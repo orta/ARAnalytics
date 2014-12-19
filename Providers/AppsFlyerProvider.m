81159df20bdb16bf6b5e500abb80289a6ec88ea7
Chris Dzombak <chris@chrisdzombak.net>
2014-12-19 13:52:13 -0500

    Add first-pass at simple AppsFlyer provider

---
+++ Providers/AppsFlyerProvider.m
@@ -0,0 +1,48 @@
+#import "AppsFlyerProvider.h"
+#import "AppsFlyerTracker.h"
+
+const NSString * ARAppsFlyerEventPropertyCurrencyCode = @"currencyCode";
+const NSString * ARAppsFlyerEventPropertyValue = @"value";
+
+@implementation AppsFlyerProvider
+#ifdef AR_APPSFLYER_EXISTS
+
+- (id)initWithIdentifier:(NSString *)identifier {
+    NSLog(@"Use -[AppsFlyerProvider initWithAppID:devKey:] instead of %s", __PRETTY_FUNCTION__);
+    [self doesNotRecognizeSelector:_cmd];
+    return nil;
+}
+
+- (instancetype)initWithAppID:(NSString *)appID devKey:(NSString *)devKey {
+    self = [super init];
+    if (!self) return nil;
+
+    AppsFlyerTracker.sharedTracker.appsFlyerDevKey = devKey;
+    AppsFlyerTracker.sharedTracker.appleAppID = appID;
+    AppsFlyerTracker.sharedTracker.isHTTPS = YES;
+
+    [AppsFlyerTracker.sharedTracker trackAppLaunch];
+
+    return self;
+}
+
+- (void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email {
+    if (userID) {
+        AppsFlyerTracker.sharedTracker.customerUserID = userID;
+    }
+}
+
+- (void)event:(NSString *)event withProperties:(NSDictionary *)properties {
+    NSString *currencyCode = properties[ARAppsFlyerEventPropertyCurrencyCode];
+    if (currencyCode) {
+        AppsFlyerTracker.sharedTracker.currencyCode = currencyCode;
+    }
+
+    if (event) {
+        NSString *value = properties[ARAppsFlyerEventPropertyValue] ?: @"";
+        [AppsFlyerTracker.sharedTracker trackEvent:event withValue:value];
+    }
+}
+
+#endif
+@end
