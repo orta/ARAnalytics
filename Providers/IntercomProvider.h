e9d3db1bbe7b1d4e9afebba77470ebb6724e4148
Orta Therox <orta.therox@gmail.com>
2015-05-27 13:39:13 -0400

    Add support for removing the event tracking from Intercom

--- Providers/IntercomProvider.h
+++ Providers/IntercomProvider.h
@@ -3,5 +3,6 @@
 @interface IntercomProvider : ARAnalyticalProvider
 
 - (instancetype)initWithWithAppID:(NSString *)identifier apiKey:(NSString *)apiKey;
+@property (nonatomic, assign) BOOL registerTrackedEvents;
 
 @end
--- Providers/IntercomProvider.m
+++ Providers/IntercomProvider.m
@@ -9,13 +9,19 @@
     NSAssert([Intercom class], @"Intercom is not included");
 
     [Intercom setApiKey:apiKey forAppId:identifier];
+    _registerTrackedEvents = YES;
 
     return [super init];
 }
 
 - (void)event:(NSString *)event withProperties:(NSDictionary *)properties
 {
-    [Intercom logEventWithName:event metaData:properties];
+    // Use ARAnalytics providerInstanceOfClass:IntercomProvider
+    // to turn this off if you're getting too many events
+
+    if (self.registerTrackedEvents) {
+        [Intercom logEventWithName:event metaData:properties];
+    }
 }
 
 - (void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email
