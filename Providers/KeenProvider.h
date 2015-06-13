18a2e4f28574affa5005f2e44715ab4cb920b2ee
Christopher Bowns <github@cbowns.com>
2015-06-12 18:01:01 -0700

    Keen: Implement event:withProperties, identifyUserWithID:andEmailAddress:, and setUserProperty:toValue

--- Providers/KeenProvider.h
+++ Providers/KeenProvider.h
@@ -4,14 +4,46 @@
 
 - (instancetype)initWithProjectID:(NSString *)projectID andWriteKey:(NSString *)writeKey andReadKey:(NSString *)readKey;
 
-// ARAnalyticalProvider methods to implement in the future:
-//- (void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email;
-//- (void)setUserProperty:(NSString *)property toValue:(NSString *)value;
-//- (void)event:(NSString *)event withProperties:(NSDictionary *)properties;
+#pragma mark - ARAnalyticalProvider implementations
+
+- (void)event:(NSString *)event withProperties:(NSDictionary *)properties;
+
+/// Sets @c userID or @c email to @c nil if a nil value is passed.
+- (void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email;
+
+- (void)setUserProperty:(NSString *)property toValue:(NSString *)value;
+
+
+// ARAnalyticalProvider methods to consider implementing in the future:
+
 //- (void)incrementUserProperty:(NSString *)counterName byInt:(NSNumber *)amount;
+
+/// Submit errors
 //- (void)error:(NSError *)error withMessage:(NSString *)message;
-//- (void)didShowNewViewController:(UIViewController *)controller;
+
+/// Monitor Navigation changes as page view
+//- (void)monitorNavigationViewController:(UINavigationController *)controller;
+
+/// Submit an event with a time interval
 //- (void)logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval;
+
+/// Submit an event with a time interval and extra properties
+/// @warning the properites must not contain the key string `length`.
+//- (void)logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval properties:(NSDictionary *)properties;
+
+/// Pass a specific event for showing a page
+//- (void)didShowNewPageView:(NSString *)pageTitle;
+
+/// Pass a specific event for showing a page with additional properties
+//- (void)didShowNewPageView:(NSString *)pageTitle withProperties:(NSDictionary *)properties;
+
+/// Submit a string to the provider's logging system
 //- (void)remoteLog:(NSString *)parsedString;
+
+/// Submit a string to the local persisted logging system
+//- (void)localLog:(NSString *)message;
+
+/// Retrieve messages provided to the local persisted logging system originating from a specified process.
+//- (NSArray *)messagesForProcessID:(NSUInteger)processID;
 
 @end
--- Providers/KeenProvider.m
+++ Providers/KeenProvider.m
@@ -1,6 +1,11 @@
 #import "KeenProvider.h"
 #import "KeenClient.h"
 
+#import <Foundation/Foundation.h>
+
+static NSString * const kKeenUserIDKey = @"user_id";
+static NSString * const kKeenEmailKey = @"email";
+
 @implementation KeenProvider
 
 - (instancetype)initWithProjectID:(NSString *)projectID andWriteKey:(NSString *)writeKey andReadKey:(NSString *)readKey {
@@ -13,7 +18,32 @@
 
 #ifdef AR_KEEN_EXISTS
 
-// Additional implementations go here.
+- (void)event:(NSString *)event withProperties:(NSDictionary *)properties
+{
+    [[KeenClient sharedClient] addEvent:properties toEventCollection:event error:nil];
+}
+
+- (void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email
+{
+    [self setUserProperty:kKeenUserIDKey toValue:userID];
+    [self setUserProperty:kKeenEmailKey toValue:email];
+}
+
+- (void)setUserProperty:(NSString *)property toValue:(NSString *)value
+{
+    KeenClient *client = [KeenClient sharedClient];
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithDictionary:client.globalPropertiesDictionary];
+
+    if (value) {
+        [dict setObject:value forKey:property];
+    } else {
+        [dict removeObjectForKey:property];
+    }
+    client.globalPropertiesDictionary = [dict copy];
+}
+
+// Additional implementation overrides, based on ARAnalyticalProvider, should follow here.
 
 #endif
+
 @end
