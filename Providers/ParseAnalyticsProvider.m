87c7f62bb9ccfb56eede037197e3a83d2acb5be1
Dmitry Obukhov <stel2k@gmail.com>
2015-08-26 11:00:12 +0300

    Code formatting cleanup

--- Providers/ParseAnalyticsProvider.m
+++ Providers/ParseAnalyticsProvider.m
@@ -1,41 +1,45 @@
+//
+//  ARAnalyticalProvider.m
+//  ARAnalytics
+//
+
 #import "ParseAnalyticsProvider.h"
 #import <Parse/Parse.h>
 
 @implementation ParseAnalyticsProvider
 
--(id)initWithApplicationID:(id)appID clientKey:(id)clientKey {
+- (instancetype)initWithApplicationID:(NSString *)applicationId clientKey:(NSString *)clientKey {
     NSAssert([Parse class], @"Parse is not included");
     
-    [Parse setApplicationId:appID
-                  clientKey:clientKey];
+    [Parse setApplicationId:applicationId clientKey:clientKey];
     
     return [super init];
 }
 
--(void)event:(NSString *)event withProperties:(NSDictionary *)properties {
-    NSString *formattedEvent=[self replacespacesWithUnderscore:event];
-    if (properties) {
+- (void)event:(NSString *)event withProperties:(NSDictionary *)properties {
+    NSString *formattedEvent = [self replacespacesWithUnderscore:event];
+    
+    if (properties != nil) {
         //values must be strings
         //https://parse.com/docs/ios/api/Classes/PFAnalytics.html#//api/name/trackEvent:dimensions:
         //quick and dirty hack to ensure this
         
-        NSMutableDictionary *dimensions = [NSMutableDictionary new];
+        NSMutableDictionary *dimensions = [NSMutableDictionary dictionary];
+        
         [properties enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-            NSString *formattedKey=[self replacespacesWithUnderscore:key];
-            NSString *formattedDescription=[self replacespacesWithUnderscore:[obj description]];
-            dimensions[formattedKey]=formattedDescription;
+            NSString *formattedKey = [self replacespacesWithUnderscore:key];
+            NSString *formattedDescription = [self replacespacesWithUnderscore:[obj description]];
+            
+            dimensions[formattedKey] = formattedDescription;
         }];
         
         [PFAnalytics trackEvent:formattedEvent dimensions:dimensions];
-        
-    }
-    else {
+    } else {
         [PFAnalytics trackEvent:formattedEvent];
     }
 }
 
--(NSString *)replacespacesWithUnderscore:(NSString *)input
-{
+- (NSString *)replacespacesWithUnderscore:(NSString *)input {
     return [input stringByReplacingOccurrencesOfString:@" " withString:@"_"];
 }
 
