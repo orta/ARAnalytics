68647aca76cf8fdd4763c6f9e6371eeea72b1a5a
Benjamin Herzog <benjamin.herzog@lovoo.com>
2016-06-22 18:49:34 +0200

    Added Leanplum support

---
+++ Providers/LeanplumProvider.h
@@ -0,0 +1,14 @@
+#import "ARAnalyticalProvider.h"
+
+@interface LeanplumProvider : ARAnalyticalProvider
+
+- (id)initWithIdentifier:(NSString *)identifier NS_UNAVAILABLE;
+
+/**
+ * Initializes an instance of LeanplumProvider with the given parameters.
+ * 
+ * NOTE: developmentKey XOR productionKey has to be set!
+*/
+- (instancetype)initWithAppId:(NSString *)appId developmentKey:(NSString *)developmentKey productionKey:(NSString *)productionKey;
+
+@end
